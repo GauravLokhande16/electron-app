edd16be3f18f77f6a211c6b1a46d0df6cee5f5de
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2025-11-20 00:47:34 +0530

    removed unnecessary code

--- electron/childSquare.js
+++
@@ -1,7 +0,0 @@
-process.on("message", (msg) => {
-  if (msg.action === "square") {
-    const result = Number(msg.number) ** 2;
-    console.log(result)
-    process.send({ type: "square-result", value: result });
-  }
-});
